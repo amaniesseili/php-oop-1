92771c336f8d4242a5bc899e831fad783020d558
amani esseili <esseiliamani@hotmail.com>
2023-09-24 18:52:07 +0200

    class movie.php

---
+++ movie.php
@@ -0,0 +1,24 @@
+<?php
+// Movie.php
+
+class Movie {
+    // Dichiarazione delle variabili di istanza
+    public $title;
+    public $director;
+    public $year;
+
+    // Costruttore
+    public function __construct($title, $director, $year) {
+        $this->title = $title;
+        $this->director = $director;
+        $this->year = $year;
+    }
+
+    // Metodo per stampare le informazioni del film
+    public function printInfo() {
+        echo "Titolo: " . $this->title . "<br>";
+        echo "Regista: " . $this->director . "<br>";
+        echo "Anno: " . $this->year . "<br>";
+    }
+}
+?>
